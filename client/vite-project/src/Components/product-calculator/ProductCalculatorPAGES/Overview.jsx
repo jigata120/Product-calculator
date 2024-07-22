a8f14c074d55d7781f6a97a5a9568fc25c7aed0e
mihaeldzhigov <pplesho@gmail.com>
2024-07-23 00:22:47 +0300

    overview component

---
+++ client/vite-project/src/Components/product-calculator/ProductCalculatorPAGES/Overview.jsx
@@ -0,0 +1,27 @@
+export default function Overview(){
+ return(
+  <>
+    <div className="content-header">
+    <div className="content-header-inner">
+        <h1 className="content-header-title">
+        Good afternoon Ryan, <br />
+        you have<small>$24,920.56</small>
+        </h1>
+        <div className="content-header-info">
+        Available funds to invest: $435.00{" "}
+        <a href="#" className="link">
+            Invest now
+        </a>
+        </div>
+    </div>
+    <div className="content-header-illustration">
+        <img src="https://assets.codepen.io/285131/illustration-2.svg" />
+    </div>
+    </div>
+    <div className="content-body">
+   
+    
+    </div>
+  </>
+ )
+}
