683a9e90fa770839908988671030f3e8de3d735e
mihaeldzhigov <pplesho@gmail.com>
2024-07-25 20:20:32 +0300

    provider added

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+ 
 // import './App.css'
+
 import AuthenticationForm from './Components/authenticate/AuthenticationForm'
 import { Route, Routes } from 'react-router-dom';
 import ProductCalculator from './Components/product-calculator/ProductCalculator';
-import NotFoundPage from './Components/NotFoundPage';
+import { UserProvider } from './contexts/UserProvider';
 
 function App() {
   const BaseUrl = ' http://localhost:3030/jsonstore'
-   
+ 
 
   return (
     <>
-    <Routes>
-      <Route path='/authentication' element={<AuthenticationForm url={BaseUrl}/>}/>
-      <Route path='/*' element={<ProductCalculator/>}/>
-       
- 
 
 
-    </Routes>
+    <UserProvider>
+        <Routes>
+           <Route path='/authentication' element={<AuthenticationForm url={BaseUrl}/>}/>
+
+            <Route path='/*' element={<ProductCalculator/>}/>
+        </Routes>
+    </UserProvider>
        
     </>
   )
