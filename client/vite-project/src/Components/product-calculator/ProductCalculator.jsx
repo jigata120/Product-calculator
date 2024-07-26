d5047ff2189a92d959ff996e66dab9d57c1674e1
mihaeldzhigov <pplesho@gmail.com>
2024-07-26 23:38:46 +0300

    nav links

--- client/vite-project/src/Components/product-calculator/ProductCalculator.jsx
+++ client/vite-project/src/Components/product-calculator/ProductCalculator.jsx
@@ -1,20 +1,32 @@
-import { Link, NavLink, Route, Routes } from "react-router-dom";
+import { Link, NavLink, Route, Routes, useNavigate } from "react-router-dom";
 import Overview from "./ProductCalculatorPAGES/Overview";
 import NotFoundPage from "../NotFoundPage";
 import Transactions from "./ProductCalculatorPAGES/Transactions";
 import Subscriptions from "./ProductCalculatorPAGES/Subscriptions";
 import style from "./ProductCalculator.jsx"
 import { useContext } from "react";
+import UserContext from '../../contexts/UserContext';
+import Wellcome from "./ProductCalculatorPAGES/Wellcome.jsx";
+import Profile from "./ProductCalculatorPAGES/profile/Profile.jsx";
+import Accounts from "./ProductCalculatorPAGES/Accounts.jsx";
+import Projects from "./ProductCalculatorPAGES/Projects.jsx";
+
+
 
 export default function ProductCalculator(){
-    const UserData  = useContext()
-    console.log(UserData)
+    // const UserData  = useContext()
+    // console.log(UserData)
+    const { user, setUser } = useContext(UserContext);
+    console.log(user);
+    const navigate = useNavigate(); 
+
     return(
         <>
             {/* partial:index.partial.html */}
+
             <div className="earmark-app">
                 <header className="header">
-                <h1 className="header-logo">earmark.</h1>
+                <h1 className="header-logo">ProductCalculator</h1>
                 <div className="header-content">
                     <div className="header-search">
                     <input type="text" className="search-field" placeholder="Search..." />
@@ -22,10 +34,33 @@ export default function ProductCalculator(){
                         <i className="ph-magnifying-glass-bold" />
                     </button>
                     </div>
+                    
                     <button className="header-avatar">
-                    <span className="header-avatar-img" />
-                    <span className="header-avatar-name">Ryan Johnson</span>
-                    </button>
+                    
+                    {user && user.length > 0 ? (
+                        <>
+                            {user[1]?.profileUrl ? (
+                            <Link to={`/user/${user[0]}`}>
+                                <img
+                                className="header-avatar-img"
+                                src={user[1]?.profileUrl}
+                                alt="Profile photo"
+                                />
+                            </Link>
+                            ) : (
+                            <Link to={`/user/${user[0]}`} className="header-avatar-span">
+                                <span className="header-avatar-span"></span>
+                            </Link>
+                            )}
+                            <Link to={`/user/${user[0]}`} className="header-avatar-name">
+                            {user[1]?.name}
+                            </Link>
+                        </>
+                        ) : (
+                        <Link to="/authentication" className="SigninSignup-btn">Sign Up/Sign In</Link>
+                        )}
+                   
+                   </button>
                 </div>
                 </header>
                 <main className="main">
@@ -47,7 +82,7 @@ export default function ProductCalculator(){
                         </NavLink>
                     </li>
                     <li className="tab">
-                        <NavLink to="bugdets" 
+                        <NavLink to="projects" 
                         className={({ isActive })=> (isActive ? "tab-btn tab-btn--active" : "tab-btn")}>
                          <i className="ph-credit-card-bold" />
                         <span className="tab-btn-title">Bugdets</span>
@@ -73,8 +108,14 @@ export default function ProductCalculator(){
                     <Routes>
                         <Route path='overview' element={<Overview/>}/>
                         <Route path='*' element={<NotFoundPage/>}/>
+                        
                         <Route path='transactions' element={<Transactions/>}/>
+                        <Route path='projects' element={<Projects/>}/>
+                        <Route path='accounts' element={<Accounts/>}/>
                         <Route path='subscriptions' element={<Subscriptions/>}/>
+                        <Route path='' element={<Wellcome name= {user[1]?.name}/>}/>
+                        <Route path='user/:userId' element={<Profile user={user}/>}/>
+                        
 
 
 
