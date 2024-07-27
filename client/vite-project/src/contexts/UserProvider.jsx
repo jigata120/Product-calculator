8bca2d1d076e2919ab8401dd201f478de416955b
mihaeldzhigov <pplesho@gmail.com>
2024-07-27 22:35:10 +0300

    CUstom Context provider

---
+++ client/vite-project/src/contexts/UserProvider.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import UserContext from './UserContext';
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({}); 
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
