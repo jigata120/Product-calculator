2a1aeaa5e2538a3814ecf1693eb23c6cac520117
mihaeldzhigov <pplesho@gmail.com>
2024-08-02 18:42:53 +0300

    basic Api requests implemented

---
+++ client/vite-project/src/Api.js
@@ -0,0 +1,89 @@
+export async function postData(url, data) {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error('Error in POST request:', error);
+        throw error;  
+    }
+}
+
+
+export async function getData(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error('Error in GET request:', error);
+        throw error;  
+    }
+}
+
+
+export async function putData(url, data) {
+    try {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error('Error in PUT request:', error);
+        throw error; 
+    }
+}
+
+
+export async function deleteData(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+ 
+        const result = response.status === 204 ? null : await response.json();
+        return result;
+    } catch (error) {
+        console.error('Error in DELETE request:', error);
+        throw error; 
+    }
+}
