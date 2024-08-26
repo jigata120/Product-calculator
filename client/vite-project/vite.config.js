d79fcc41d9c4ac330ce440e70f7286fc425c6ef1
Mihaeldzh <pplesho@gmail.com>
2024-08-26 18:32:27 +0300

    tests configs

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://product-calculator-1-wu2x.onrender.com'
+  base: 'https://product-calculator-1-wu2x.onrender.com',
+  test:{
+    environment: 'jsdom'
+  },
+  setupFiles:'./tests/setup.js'
 })
